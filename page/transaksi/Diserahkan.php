3e32ab7fe6715ad9b508517c0ff666d2d25263c6
CahyaHayyuni <cahyahafidzah1@gmail.com>
2021-03-18 14:59:14 +0800

    sdh diganti

--- page/transaksi/Diserahkan.php
+++ page/transaksi/Diserahkan.php
@@ -4,13 +4,13 @@ $id = $_GET['id'];
 $judul = $_GET['judul'];
 
 
-$sql = $koneksi->query("update tb_transaksi set status='kembali' where id='$id'");
+$sql = $koneksi->query("update tb_transaksi set status='Diserahkan' where id='$id'");
 
 $sql = $koneksi->query("update tb_buku set jumlah_buku = (jumlah_buku+1) where judul='$judul'");
 
 ?>
 <script type="text/javascript">
-    alert("Proses Kembalikan Buku Berhasil");
+    alert("Proses Diserahkan Barang Berhasil");
     window.location.href = "?page=transaksi";
 </script>
 <?php
